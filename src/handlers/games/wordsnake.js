0fdd093e3060c31528b42785945f0fecbfbbfb2f
Sanat Wadyalkar <sanat.wadyalkar.cse@ghru.edu.in>
2025-11-12 15:43:44 +0530

    Fixed same again

--- src/handlers/games/wordsnake.js
+++ src/handlers/games/wordsnake.js
@@ -1,44 +1,42 @@
-const Discord = require('discord.js');
-
+const Discord = require("discord.js");
 const Schema = require("../../database/models/wordsnake");
 
 module.exports = async (client) => {
-  client.on(Discord.Events.MessageCreate, async (message) => {
-    if (message.author.bot || message.channel.type === Discord.ChannelType.DM) return;
+  client
+    .on(Discord.Events.MessageCreate, async (message) => {
+      if (message.author.bot || message.channel.type === Discord.ChannelType.DM)
+        return;
 
-    Schema.findOne({ Guild: message.guild.id, Channel: message.channel.id }, async (err, data) => {
-      if (data) {
-        try {
-          if (!data.lastWord || data.lastWord == " ") {
-            message.react(client.emotes.normal.check);
+      try {
+        const data = await Schema.findOne({
+          Guild: message.guild.id,
+          Channel: message.channel.id,
+        });
 
-            var word = message.content;
+        if (data) {
+          if (!data.lastWord || data.lastWord.trim() === "") {
+            await message.react(client.emotes.normal.check);
 
-            data.lastWord = word;
-            data.save();
+            data.lastWord = message.content;
+            await data.save();
             return;
           }
 
-          const result = data.lastWord.split('');
-          let number = result.length - 1;
+          const result = data.lastWord.split("");
+          const lastChar = result[result.length - 1]?.toLowerCase();
 
-          if (message.content.toLowerCase().startsWith(result[number].toLowerCase())) {
-            message.react(client.emotes.normal.check);
+          if (message.content.toLowerCase().startsWith(lastChar)) {
+            await message.react(client.emotes.normal.check);
 
-            var word = message.content.replace(" ", "");
-
-            data.lastWord = word;
-            data.save();
-            return;
-          }
-          else {
-            return message.react(client.emotes.normal.error);
+            data.lastWord = message.content.replace(/\s+/g, "");
+            await data.save();
+          } else {
+            await message.react(client.emotes.normal.error);
           }
         }
-        catch (err) {
-          console.log(err);
-        }
+      } catch (err) {
+        console.error("[WordSnake] Error:", err);
       }
     })
-  }).setMaxListeners(0);
-}
+    .setMaxListeners(0);
+};
