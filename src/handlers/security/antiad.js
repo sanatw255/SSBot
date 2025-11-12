a91a5f71cc500a3db7589ba09c0f52b033bb1b6e
Sanat Wadyalkar <sanat.wadyalkar.cse@ghru.edu.in>
2025-11-12 15:48:03 +0530

    Fixed same again

--- src/handlers/security/antiad.js
+++ src/handlers/security/antiad.js
@@ -1,189 +1,174 @@
 const Discord = require("discord.js");
-
 const Schema = require("../../database/models/functions");
 const Schema2 = require("../../database/models/channelList");
 
 module.exports = (client) => {
-    client.on(Discord.Events.MessageCreate, async (message) => {
-        if (message.channel.type === Discord.ChannelType.DM || message.author.bot) return;
-        Schema.findOne({ Guild: message.guild.id }, async (err, data) => {
-            if (data) {
-                if (data.AntiInvite == true) {
-                    const { content } = message
+  client
+    .on(Discord.Events.MessageCreate, async (message) => {
+      if (message.channel.type === Discord.ChannelType.DM || message.author.bot)
+        return;
 
-                    const code = content.split('discord.gg/')[1]
-                    if (code) {
-                        Schema2.findOne({ Guild: message.guild.id }, async (err, data2) => {
-                            if (data2) {
-                                if (data2.Channels.includes(message.channel.id) || message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) {
-                                    return;
-                                }
+      try {
+        const data = await Schema.findOne({ Guild: message.guild.id });
+        if (!data) return;
 
-                                message.delete();
+        // 🔗 Anti-Invite System
+        if (data.AntiInvite === true) {
+          const { content } = message;
+          const code = content.split("discord.gg/")[1];
 
-                                client.embed({
-                                    title: `${client.emotes.normal.error}・Moderator`,
-                                    desc: `Discord links are not allowed in this server!`,
-                                    color: client.config.colors.error,
-                                    content: `${message.author}`
-                                }, message.channel)
-                            }
-                            else {
-                                if (message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) return;
-                                message.delete();
+          if (code) {
+            const data2 = await Schema2.findOne({ Guild: message.guild.id });
+            const exempt =
+              data2 &&
+              (data2.Channels.includes(message.channel.id) ||
+                message.member.permissions.has(
+                  Discord.PermissionsBitField.Flags.ManageMessages
+                ));
 
-                                client.embed({
-                                    title: `${client.emotes.normal.error}・Moderator`,
-                                    desc: `Discord links are not allowed in this server!`,
-                                    color: client.config.colors.error,
-                                    content: `${message.author}`
-                                }, message.channel)
-                            }
-                        })
-                    }
-                }
-                else if (data.AntiLinks == true) {
-                    const { content } = message
+            if (exempt) return;
 
-                    if (content.includes('http://') || content.includes('https://') || content.includes('www.')) {
-                        Schema2.findOne({ Guild: message.guild.id }, async (err, data2) => {
-                            if (data2) {
-                                if (data2.Channels.includes(message.channel.id) || message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) {
-                                    return;
-                                }
+            await message.delete();
+            await client.embed(
+              {
+                title: `${client.emotes.normal.error}・Moderator`,
+                desc: `Discord links are not allowed in this server!`,
+                color: client.config.colors.error,
+                content: `${message.author}`,
+              },
+              message.channel
+            );
+          }
+        }
 
-                                message.delete();
+        // 🌐 Anti-Link System
+        else if (data.AntiLinks === true) {
+          const { content } = message;
+          if (
+            content.includes("http://") ||
+            content.includes("https://") ||
+            content.includes("www.")
+          ) {
+            const data2 = await Schema2.findOne({ Guild: message.guild.id });
+            const exempt =
+              data2 &&
+              (data2.Channels.includes(message.channel.id) ||
+                message.member.permissions.has(
+                  Discord.PermissionsBitField.Flags.ManageMessages
+                ));
 
-                                client.embed({
-                                    title: `${client.emotes.normal.error}・Moderator`,
-                                    desc: `Links are not allowed in this server!`,
-                                    color: client.config.colors.error,
-                                    content: `${message.author}`
-                                }, message.channel)
-                            }
-                            else {
-                                if (message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) return;
-                                message.delete();
-                               
-                                client.embed({
-                                    title: `${client.emotes.normal.error}・Moderator`,
-                                    desc: `Links are not allowed in this server!`,
-                                    color: client.config.colors.error,
-                                    content: `${message.author}`
-                                }, message.channel)
-                            }
-                        })
-                    }
-                }
-            }
-        })
-    }).setMaxListeners(0);
+            if (exempt) return;
 
-    client.on(Discord.Events.MessageUpdate, async (oldMessage, newMessage) => {
-        if (oldMessage.content === newMessage.content || newMessage.channel.type === Discord.ChannelType.DM) return;
+            await message.delete();
+            await client.embed(
+              {
+                title: `${client.emotes.normal.error}・Moderator`,
+                desc: `Links are not allowed in this server!`,
+                color: client.config.colors.error,
+                content: `${message.author}`,
+              },
+              message.channel
+            );
+          }
+        }
+      } catch (err) {
+        console.error("[AntiAd] MessageCreate error:", err);
+      }
+    })
+    .setMaxListeners(0);
 
-        Schema.findOne({ Guild: newMessage.guild.id }, async (err, data) => {
-            if (data) {
-                if (data.AntiInvite == true) {
-                    const { content } = newMessage
+  // 📝 Handle Message Updates
+  client
+    .on(Discord.Events.MessageUpdate, async (oldMessage, newMessage) => {
+      if (
+        oldMessage.content === newMessage.content ||
+        newMessage.channel.type === Discord.ChannelType.DM
+      )
+        return;
 
-                    const code = content.split('discord.gg/')[1]
-                    if (code) {
-                        Schema2.findOne({ Guild: newMessage.guild.id }, async (err, data2) => {
-                            if (data2) {
-                                if (data2.Channels.includes(newMessage.channel.id) || newMessage.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) {
-                                    return;
-                                }
+      try {
+        const data = await Schema.findOne({ Guild: newMessage.guild.id });
+        if (!data) return;
 
-                                newMessage.delete();
-                                let error = new Discord.EmbedBuilder()
-                                    .setTitle(`${client.emotes.normal.error}・Moderator`)
-                                    .setAuthor(client.user.username, client.user.avatarURL())
-                                    .setDescription(`Discord links are not allowed in this server!`)
-                                    .setColor(client.config.colors.error)
-                                    .setFooter({ text: client.config.discord.footer })
-                                    .setTimestamp();
-                                var msg = newMessage.channel.send({ content: `${newMessage.author}`, embeds: [error] })
-                                setTimeout(() => {
-                                    try{
-                                        msg.delete();
-                                    } catch (e) {
-                                        return;
-                                    }
-                                }, 5000)
-                            }
-                            else {
-                                if (newMessage.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) return;
-                                newMessage.delete();
-                                let error = new Discord.EmbedBuilder()
-                                    .setTitle(`${client.emotes.normal.error}・Moderator`)
-                                    .setAuthor(client.user.username, client.user.avatarURL())
-                                    .setDescription(`Discord links are not allowed in this server!`)
-                                    .setColor(client.config.colors.error)
-                                    .setFooter({ text: client.config.discord.footer })
-                                    .setTimestamp();
-                                var msg = newMessage.channel.send({ content: `${newMessage.author}`, embeds: [error] })
-                                setTimeout(() => {
-                                    try {
-                                        msg.delete();
-                                    } catch (e) {
-                                        return;
-                                    }
-                                }, 5000)
-                            }
-                        })
-                    }
-                }
-                else if (data.AntiLinks == true) {
-                    const { guild, member, content } = newMessage
+        // 🔗 Anti-Invite on Edited Messages
+        if (data.AntiInvite === true) {
+          const { content } = newMessage;
+          const code = content.split("discord.gg/")[1];
 
-                    if (content.includes('http://') || content.includes('https://') || content.includes('www.')) {
-                        Schema2.findOne({ Guild: newMessage.guild.id }, async (err, data2) => {
-                            if (data2) {
-                                if (data2.Channels.includes(newMessage.channel.id) || newMessage.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) {
-                                    return;
-                                }
+          if (code) {
+            const data2 = await Schema2.findOne({ Guild: newMessage.guild.id });
+            const exempt =
+              data2 &&
+              (data2.Channels.includes(newMessage.channel.id) ||
+                newMessage.member.permissions.has(
+                  Discord.PermissionsBitField.Flags.ManageMessages
+                ));
 
-                                newMessage.delete();
-                                var error = new Discord.EmbedBuilder()
-                                    .setTitle(`${client.emotes.normal.error}・Moderator`)
-                                    .setAuthor(client.user.username, client.user.avatarURL())
-                                    .setDescription(`Links are not allowed in this server!`)
-                                    .setColor(client.config.colors.error)
-                                    .setFooter({ text: client.config.discord.footer })
-                                    .setTimestamp();
-                                var msg = newMessage.channel.send({ content: `${newMessage.author}`, embeds: [error] })
-                                setTimeout(() => {
-                                    try {
-                                        msg.delete();
-                                    } catch (e) {
-                                        return;
-                                    }
-                                }, 5000)
-                            }
-                            else {
-                                if (newMessage.member.permissions.has(Discord.PermissionsBitField.Flags.ManageMessages)) return;
-                                newMessage.delete();
-                                var error = new Discord.EmbedBuilder()
-                                    .setTitle(`${client.emotes.normal.error}・Moderator`)
-                                    .setAuthor(client.user.username, client.user.avatarURL())
-                                    .setDescription(`Links are not allowed in this server!`)
-                                    .setColor(client.config.colors.error)
-                                    .setFooter({ text: client.config.discord.footer })
-                                    .setTimestamp();
-                                var msg = newMessage.channel.send({ content: `${newMessage.author}`, embeds: [error] })
-                                setTimeout(() => {
-                                    try {
-                                        msg.delete();
-                                    } catch (e) {
-                                        return;
-                                    }
-                                }, 5000)
-                            }
-                        })
-                    }
-                }
-            }
-        })
-    }).setMaxListeners(0);
-}
+            if (exempt) return;
+
+            await newMessage.delete();
+
+            const error = new Discord.EmbedBuilder()
+              .setTitle(`${client.emotes.normal.error}・Moderator`)
+              .setAuthor({
+                name: client.user.username,
+                iconURL: client.user.avatarURL(),
+              })
+              .setDescription(`Discord links are not allowed in this server!`)
+              .setColor(client.config.colors.error)
+              .setFooter({ text: client.config.discord.footer })
+              .setTimestamp();
+
+            const msg = await newMessage.channel.send({
+              content: `${newMessage.author}`,
+              embeds: [error],
+            });
+            setTimeout(() => msg.delete().catch(() => {}), 5000);
+          }
+        }
+
+        // 🌐 Anti-Link on Edited Messages
+        else if (data.AntiLinks === true) {
+          const { content } = newMessage;
+
+          if (
+            content.includes("http://") ||
+            content.includes("https://") ||
+            content.includes("www.")
+          ) {
+            const data2 = await Schema2.findOne({ Guild: newMessage.guild.id });
+            const exempt =
+              data2 &&
+              (data2.Channels.includes(newMessage.channel.id) ||
+                newMessage.member.permissions.has(
+                  Discord.PermissionsBitField.Flags.ManageMessages
+                ));
+
+            if (exempt) return;
+
+            await newMessage.delete();
+
+            const error = new Discord.EmbedBuilder()
+              .setTitle(`${client.emotes.normal.error}・Moderator`)
+              .setAuthor({
+                name: client.user.username,
+                iconURL: client.user.avatarURL(),
+              })
+              .setDescription(`Links are not allowed in this server!`)
+              .setColor(client.config.colors.error)
+              .setFooter({ text: client.config.discord.footer })
+              .setTimestamp();
+
+            const msg = await newMessage.channel.send({
+              content: `${newMessage.author}`,
+              embeds: [error],
+            });
+            setTimeout(() => msg.delete().catch(() => {}), 5000);
+          }
+        }
+      } catch (err) {
+        console.error("[AntiAd] MessageUpdate error:", err);
+      }
+    })
+    .setMaxListeners(0);
+};
